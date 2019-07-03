f47f85c8d921d219f0e71bb58d702676ec7ec1d7
Nithesh Kg <nitheshkg18@gmail.com>
2019-07-03 08:56:30 +0530

    test ng commit

--- src/test/java/stepdef/Sample1.java
+++ src/test/java/stepdef/Sample1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.BeforeTest;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
