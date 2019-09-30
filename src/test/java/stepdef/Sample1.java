8b4f9b2d8c20cfbc89b87616decdf0f9452fade6
Nithesh Kg <nitheshkg18@gmail.com>
2019-09-30 16:09:21 +0530

    Uncommented the important functionality

--- src/test/java/stepdef/Sample1.java
+++ src/test/java/stepdef/Sample1.java
@@ -19,27 +19,27 @@ public class Sample1 {
 	@Given("user opens login page in newtours application")
 	public void user_opens_login_page_in_newtours_application() {
 	   System.out.println("user opened login page in newtours application");
-	   /*System.setProperty("webdriver.chrome.driver","C:\\New folder (5)\\chromedriver_win32\\chromedriver.exe");
+	   System.setProperty("webdriver.chrome.driver","C:\\New folder (5)\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
-	   driver.get("http://newtours.demoaut.com/mercurysignon.php");*/
+	   driver.get("http://newtours.demoaut.com/mercurysignon.php");
 	}
 
 	@When("user enters the username as mercury and password as mercury")
 	public void user_enters_the_username_as_mercury_and_password_as_mercury() {
-		/*
+		
 		WebElement userid= driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[1]/td[2]/input"));
 		userid.sendKeys("mercury");
 		WebElement password = driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input"));
 		password.sendKeys("mercury");
-	*/	
+		
 	   System.out.println("user entered the username as mercury and password as mercury");
 	}
 
 	@When("Click Submit button")
 	public void click_Submit_button() {
-	/*	WebElement submit=driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input"));
+		WebElement submit=driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input"));
 		submit.click();
-	*/	System.out.println("submit button clicked");
+		System.out.println("submit button clicked");
 	}
 
 	@Then("verify login success")
